f1be994a4279cd7c3960215e99ff0ce480095e23
Rocio-Peralta <1rocioperalta@gmail.com>
2023-07-31 15:32:11 +1200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,72 @@
-function App () {
+import { useRef, useState } from 'react'
+import { getActiveToken } from './utils/getActiveToken'
+import { useSearchBox } from 'react-instantsearch-hooks'
+import { Autocomplete } from './Autocomplete'
+import getCaretCoordinates from 'textarea-caret'
+
+function App() {
+  const inputRef = useRef()
+  const [showAutocComplete, setShowAutocComplete] = useState(false)
+  const { refine } = useSearchBox()
+
+  const { top, height } = inputRef.current
+    ? getCaretCoordinates(inputRef.current, inputRef.current.selectionEnd)
+    : { top: 0, height: 0 }
+
+  const handleInput = () => {
+    const { value, selectionEnd = 0 } = inputRef.current
+    const { word } = getActiveToken(value, selectionEnd)
+    const shouldOpenAutoComplete = /^@\w{1,15}$/.test(word)
+    setShowAutocComplete(shouldOpenAutoComplete)
+    shouldOpenAutoComplete && refine(word.slice(1))
+  }
+
+  const handleSelection = (userHandle) => {
+    const { value, selectionEnd = 0 } = inputRef.current
+    const { word, range } = getActiveToken(value, selectionEnd)
+    const [index] = range
+
+    const prefix = value.substring(0, index)
+    const suffix = value.substring(index + word.length)
+
+    const newText = prefix + `@${userHandle}` + suffix
+
+    inputRef.current.value = newText
+    inputRef.current.focus()
+
+    setShowAutocComplete(false)
+  }
   return (
-    <main className='container'>
-
-      <section className='box'>
-        <div className='box-body'>
-
-          <aside className='box-avatar'>
-            <img src='https://unavatar.io/twitter/midudev' alt='midudev' />
+    <main className="container">
+      <section className="box">
+        <div className="box-body">
+          <aside className="box-avatar">
+            <img src="/rocio.jpeg" alt="rocio" />
           </aside>
 
-          <div className='box-compose'>
+          <div className="box-compose">
             <form>
               <textarea
-                placeholder='¿Qué está pasando?'
-                className='box-textbox'
-                onKeyUp={() => {}}
+                placeholder="¿Qué está pasando?"
+                className="box-textbox"
+                onKeyUp={handleInput}
                 onClick={() => {}}
+                ref={inputRef}
               />
             </form>
-
+            {showAutocComplete && (
+              <Autocomplete
+                handleSelection={handleSelection} top = {`${top + height}px`}
+              />
+            )}
           </div>
         </div>
 
-        <footer className='box-footer'>
-          <button type='submit' className='tweet-button'>
+        <footer className="box-footer">
+          <button type="submit" className="tweet-button">
             Twittear
           </button>
         </footer>
-
       </section>
     </main>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,18 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+import algoliasearch from 'algoliasearch'
+import { InstantSearch } from 'react-instantsearch-hooks'
+
+const searchClient = algoliasearch(
+  'latency',
+  'a4390aa69f26de2fd0c4209ff113a4f9'
+)
+const INDEX_NAME = 'autocomplete_twitter_accounts'
+
 ReactDOM.render(
-  <App />,
+  <InstantSearch searchClient={searchClient} indexName={INDEX_NAME}>
+    <App />
+  </InstantSearch>,
   document.getElementById('root')
 )
